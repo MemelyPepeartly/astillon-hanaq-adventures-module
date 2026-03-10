4a3b80813b8bb0c8821c4009443dedaad117315a
MemelyPepeartly <memelypepeartly@gmail.com>
2026-03-10 02:11:58 -0400

    Add foundry-shims.d.ts for type declarations and interface definitions

---
+++ src/types/foundry-shims.d.ts
@@ -0,0 +1,19 @@
+declare const Hooks: any;
+declare const Dialog: any;
+declare const ChatMessage: any;
+declare const foundry: any;
+declare const game: any;
+declare const canvas: any;
+declare const ui: any;
+declare const loadTemplates: any;
+declare const jQuery: any;
+declare const $: any;
+declare const gsap: any;
+
+interface Math {
+  clamped(value: number, min: number, max: number): number;
+}
+
+interface Window {
+  gsap?: any;
+}
